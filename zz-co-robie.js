8cba15cc41a81fd1eaa4e70fff652b249b1cf7cb
Joanna-Golofit <sunderlajsa@gmail.com>
2021-09-20 11:52:15 +0200

    zad 1 dziala

---
+++ zz-co-robie.js
@@ -0,0 +1,16 @@
+konsola:
+npm install simplelightbox
+npm run dev
+
+
+
+
+
+
+
+
+do gallery 1
+
+import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
++ caly kod js do 2 zadania s simple a nie z basic
