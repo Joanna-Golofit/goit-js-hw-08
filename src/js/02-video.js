fb3dbdec7278701319c28e866ba3630429996cb7
Joanna-Golofit <sunderlajsa@gmail.com>
2021-09-21 14:31:32 +0200

    zad 2 dziala

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,79 @@
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle((e)=> {
+  // console.log('timeupdate the video!', e.seconds);
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+}, 1000),
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+
+
+
+/// --------------- zgromadzone dane ----------
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
+
+
+// // throttle
+// document.addEventListener(
+//   'scroll',
+//   _.throttle(() => {
+//     console.log('Scroll handler call every 300ms');
+//   }, 300),
+// );
+
+
+//----------
+
+// Zbadaj dokumentację metody on() i zacznij śledzić zdarzenie timeupdate - aktualizacja czasu odtwarzania.
+// Zapisuj czas odtwarzania w local storage. Niech kluczem do storage będzie "videoplayer-current-time".
+// Do przeładowywania strony używaj metody setCurrentTime() aby wznowić odtwarzanie od zapisanego momentu.
+// Dodaj do projektu bibliotekę lodash.throttle i zrób tak, aby czas odtwarzania aktualizował się w storage nie częściej niż raz na sekundę.
+
+// //------------------- on()
+
+// on(event: string, callback: function): void
+// Add an event listener for the specified event. Will call the callback with a single parameter, data, that contains the data for that event. See events below for details.
+
+// var onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+// //--------------event ---- timeupdate
+
+// Triggered as the currentTime of the video updates. It generally fires every 250ms, but it may vary depending on the browser.
+
+// {
+//     duration: 61.857
+//     percent: 0.049
+//     seconds: 3.034
+// }
+
+//---------- setCurrentTime
+
+// setCurrentTime(seconds: number): Promise<number, (RangeError|Error)>
+// Set the current playback position in seconds. Once playback has started, if the player was paused, it will remain paused. Likewise, if the player was playing, it will resume playing once the video has buffered. Setting the current time before playback has started will cause playback to start.
+
+// You can provide an accurate time and the player will attempt to seek to as close to that time as possible. The exact time will be the fulfilled value of the promise.
+
+// player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
--- zz-co-robie.js
+++ zz-co-robie.js
@@ -1,16 +1,44 @@
-konsola:
+za kazdym razem zeby sie https pokazalo trzeba
+npm run dev
+  ?
+  
+
+  skad wiedziec jak importy maja wygladac ? co konkretnie importowac ? i jak ?
+  w https ://github.com/vimeo/player.js/blob/master/src/player.js
+  widze tylko 1 export: export default Player;  
+
+
+
+
+do zad 1 - konsola:
 npm install simplelightbox
 npm run dev
 
 
 
-
-
-
-
-
 do gallery 1
 
-import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-+ caly kod js do 2 zadania s simple a nie z basic
+  import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
+  import 'simplelightbox/dist/simple-lightbox.min.css';
+  + caly kod js do 2 zadania s simple a nie z basic
+
+zmiana github pages i w package.json
+
+
+
+
+======== do zad 2
+na drugim oknie terminala (bo w  pierwszym nie dalo sie pisac) wpisalam:
+  npm install @vimeo/player
+
+  widac w package.json, ze dodalo sie w dependencies
+
+
+  lodash i throttle - w konsoli 2 polecenia
+  npm i -g npm
+  npm i lodash
+
+  a potem te: bo chyba dalam złe
+  {sudo -H} npm i -g npm          - nie zadzialalo przez {} ale bez niego to to samo co zrobilam
+  npm i --save lodash.throttle
+
