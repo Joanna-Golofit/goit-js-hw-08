2fa889357b69d980d056a7b40ade1e68669f08dd
Joanna-Golofit <sunderlajsa@gmail.com>
2021-09-22 14:33:22 +0200

    działało zad 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,44 +3,51 @@ var throttle = require('lodash.throttle');
 const form = document.querySelector('.feedback-form');
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 
-// zapisac te dane jako obiekt, przyklad:
-const data = {
-  email: form.elements.email.value,
-  message: form.elements.message.value,
-}
-const dataJSON = JSON.stringify(data);
-
-
-
-//=======================moje dziala troche ========================================================
 //na dzien dobry sprawdzanie czy nie ma danych zapisanych w localStorage:
 updateForm();
 
 //na kazda zmiane w polach formularza zapisujemy dane w local storage:
 // form.addEventListener('input', saveEmailAndMessage);
-//co 2s na zmiane w polach formularza zapisujemy dane w local storage:
-form.addEventListener('input', throttle(saveEmailAndMessage,2000));
+//co 0,5s na zmiane w polach formularza zapisujemy dane w local storage:
+form.addEventListener('input', throttle(saveEmailAndMessage, 500));
+
 
 function saveEmailAndMessage(evt) {
+  // zapisac te dane jako obiekt:
+  const data = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  //zmienic w JSON:
+  const dataJSON = JSON.stringify(data);
   //zapisanie danych w localStorage:
-  localStorage.setItem('email.value', form.elements.email.value);
-  localStorage.setItem('message.value', form.elements.message.value);
-
+  // localStorage.setItem('email.value', form.elements.email.value);
+  // localStorage.setItem('message.value', form.elements.message.value);
   localStorage.setItem(LOCALSTORAGE_KEY, dataJSON);
 
-  // localStorage.setItem(LOCALSTORAGE_KEY, dataJSON);
-  console.log("1 data", data);
-  console.log("2 dataJSON", dataJSON);
+  //logi moje
+  // console.log("1 data", data);
+  // console.log('1 data.email', data.email);
+  // console.log('form.elements.email.value:', form.elements.email.value);  
+  // console.log("2 dataJSON", dataJSON);
   console.log("3 json from storage", localStorage.getItem(LOCALSTORAGE_KEY));
-  console.log("===========");
-  updateForm();
+  // console.log("===========");
+
+  // updateForm();
   // form.reset();
 }
 
 function updateForm() {
-  form.elements.email.value = localStorage.getItem('email.value') || '';
+  console.log('wraca JSON.parse', JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+
+  form.elements.email.value = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).email || '';
   form.elements.message.value =
-  localStorage.getItem('message.value') || '';
+    JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)).message || '';
+  // form.elements.email.value = localStorage.getItem(LOCALSTORAGE_KEY.email) || '';
+  
+  //nie-obiekt
+  // form.elements.email.value = localStorage.getItem('email.value') || '';
+  // form.elements.message.value = localStorage.getItem('message.value') || '';
 }
 
 form.addEventListener('submit', onSubmit);
@@ -49,11 +56,13 @@ function onSubmit(evt) {
   //nie przeladuje strony z formularzem po kliknieciu submit:
   evt.preventDefault();
   //wyprowadzanie danych do wiersza polecen:
-  console.log(localStorage.getItem('email.value'));
-  console.log(localStorage.getItem('message.value'));
-  //czyszczenie local storage(rowniez obiektu):
-  localStorage.removeItem('email.value');
-  localStorage.removeItem('message.value');
+  // console.log(localStorage.getItem('email.value'));
+  // console.log(localStorage.getItem('message.value'));
+  console.log('JSON from localStorage', localStorage.getItem(LOCALSTORAGE_KEY));
+  console.log('Parsed JSON from localStorage', JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)));
+  //czyszczenie local storage:
+  // localStorage.removeItem('email.value');
+  // localStorage.removeItem('message.value');
   localStorage.removeItem(LOCALSTORAGE_KEY);
   //resetowanie formularza:
   form.reset();
